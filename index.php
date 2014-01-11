b4f72de14b5b24f319edbc869f7154fe0f0c694c
Allan Conda <allan.conda@gmail.com>
2014-01-11 16:34:27 +0800

    added index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: isnalla
+ * Date: 1/11/14
+ * Time: 4:33 PM
+ */
+?>
+<html>
+    <head>
+
+    </head>
+    <body>
+        ang pogi ni allan
+        ang ganda ni bianca
+    </body>
+</html>
