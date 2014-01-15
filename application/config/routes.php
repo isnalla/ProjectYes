6cb1bb082e8e14682a001dbec34be78941dda0b9
akosikiang <bpdmerced@gmail.com>
2014-01-15 22:36:08 +0800

    latest manage view

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,7 +38,7 @@
 |
 */
 
-$route['default_controller'] = "welcome";
+$route['default_controller'] = "welcome"; // change this
 $route['404_override'] = '';
 
 
--- application/views/manage_view.php
+++ application/views/manage_view.php
@@ -11,6 +11,19 @@
     <title><?php echo $title ?></title>
 </head>
 <body>
+    <div>
+         <!--table of books-->
+    </div>
+    <form>
+        <input type="text" name="book_no" id="book_no"/>
+        <input type="text" name="book_title" id="book_title"/>
+        <input type="text" name="status" id="status"/>
+        <input type="text" name="description" id="description"/>
+        <input type="text" name="publisher" id="publisher"/>
+        <input type="text" name="date_published" id="date_published"/>
+        <input type="submit" name="submit" id="submit">
+    </form>
+
 
 </body>
 </html>
