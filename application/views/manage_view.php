ea8c18852b80d6522c5d2d4d4d7ce6fdb89203cd
raphaelcosuco <cosucoraphael@gmail.com>
2014-02-12 20:37:55 +0700

    added tag element and php validation for ADD.

--- application/views/manage_view.php
+++ application/views/manage_view.php
@@ -15,10 +15,10 @@
             <td><input type="text" name="book_no" id="add_book_no" placeholder="Book Number" required autofocus pattern="[A-Za-z0-9]+"></td>
             <td><input type="text" name="book_title" id="add_book_title" placeholder="Book Title" required pattern="[-A-Za-z0-9]+"/></td>
             <td><input type="text" name="author" id="add_author" placeholder="Author" pattern="[A-Za-z]+[-]?[A-Za-z]+"/></td>
-            <td><textarea name="description" id="add_description" placeholder="Description" required ></textarea></td>
-            <td><input type="text" name="publisher" id="add_publisher" placeholder="Publisher" required /></td>
-            <td><input type="date" name="date_published" id="add_date_published" placeholder="Date Published" required/></td>
-            <td><input type="text" name="tags" id="add_tags" placeholder="Tags"required pattern="^[-a-zA-Z0-9]+{\,[a-zA-Z0-9]+}*"/></td>
+            <td><textarea name="description" id="add_description" placeholder="Description"  ></textarea></td>
+            <td><input type="text" name="publisher" id="add_publisher" placeholder="Publisher"  /></td>
+            <td><input type="date" name="date_published" id="add_date_published" placeholder="Date Published" /></td>
+            <td><input type="text" name="tags" id="add_tags" placeholder="Tags" pattern="^[-a-zA-Z0-9]+{\,[a-zA-Z0-9]+}*"/></td>
             <td><button type="submit" name="add_button" id="add_button">Add Button</button></td>
         </tr>
         <tr>
@@ -39,10 +39,10 @@
         <legend>Personal Information</legend>
 
         <label for="book_no">Book No: </label>
-        <input type="text" name="book_no" id="edit_book_no" /><span name="help_book_no"> </span>
+        <input type="text" name="book_no" id="edit_book_no" required autofocus pattern="[A-Za-z0-9]+ /><span name="help_book_no"> </span>
         </br>
         <label for="book_title">Book Title: </label>
-        <input type="text" name="book_title" id="edit_book_title" /><span name="help_book_title"> </span>
+        <input type="text" name="book_title" id="edit_book_title" required pattern="[-A-Za-z0-9]+ /><span name="help_book_title"> </span>
         </br>
 
         <label for="book_status">Book Status: </label>
@@ -55,16 +55,16 @@
         <br />
 
         <label for="description">Book Description: </label></br>
-        <textarea name="description" id="edit_description"size=50 maxlength=255></textarea><span name="help_book_description"> </span><br />
+        <textarea name="description" id="edit_description"size=50 maxlength=255 placeholder="Description"></textarea><span name="help_book_description"> </span><br />
 
         <label for="publisher">Book Publisher: </label>
-        <input type="text" name="publisher" id="edit_publisher" /><span name="help_book_publisher"> </span><br />
+        <input type="text" name="publisher" id="edit_publisher" placeholder="Publisher" /><span name="help_book_publisher"> </span><br />
 
         <label for="author">Book Author: </label>
-        <input type="text" name="author" id="edit_author" /><span name="help_book_author"> </span>
+        <input type="text" name="author" id="edit_author"  pattern="[A-Za-z]+[-]?[A-Za-z]+ /><span name="help_book_author"> </span>
         </br>
         Tags:
-        <input type="text" name="tags" id= "edit_tags"/><span name="help_tags"></span><br/>
+        <input type="text" name="tags" id= "edit_tags"placeholder="Tags"required pattern="^[-a-zA-Z0-9]+{\,[a-zA-Z0-9]+}*/><span name="help_tags"></span><br/>
         Date Published:
         <input type="date" name="date_published" id="edit_date_published" /> <span name="help_date_published"> </span>
         <br/>
